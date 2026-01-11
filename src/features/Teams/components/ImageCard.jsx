ffa9bdcfcceb13ff0228322fc665214d70663ef9
Milad Azizani <miladazizani@gmail.com>
2026-01-11 13:57:07 +0430

    Change Team Card to Components

--- src/features/Teams/components/ImageCard.jsx
+++ src/features/Teams/components/ImageCard.jsx
@@ -1,7 +1,17 @@
-const ImageCard = () => {
-    return(
-        <>
-        </>
+import { Button } from "antd"
+
+const ImageCard = ({name, job, src}) => {
+    return (
+        <div className=" h-120">
+            <div className="relative group cursor-pointer">
+                <img src={src} className="h-110 rounded-2xl border-2 border-black" />
+                <div className="absolute inset-0 top-60 text-white overlay-1 bg-[#00000081] text-center  rounded-b-2xl rounded-t-xl  text-2xl opacity-0 h-50  transition-opacity group-hover:opacity-100 flex flex-col gap-5 justify-center items-center">
+                    <p className="text-2xl font-bold">{name}</p>
+                    <p className="text-xl font-semibold">{job}</p>
+                    <Button className="h-30 w-30 bg-red-600! font-bold!" type="primary">See Profile</Button>
+                </div>
+            </div>
+        </div>
     )
 }
 export default ImageCard
--- src/features/Teams/pages/Team.jsx
+++ src/features/Teams/pages/Team.jsx
@@ -1,4 +1,5 @@
 import { Button } from "antd"
+import ImageCard from "../components/ImageCard"
 
 const Team = () => {
     return (
@@ -10,47 +11,12 @@ const Team = () => {
                 </div>
             </section>
             <section className="h-8/12 w-full grid grid-cols-4 gap-6">
-                <div className=" h-120">
-                    
-                    <div className="relative group cursor-pointer">
-                        <img src="./images/milad.jpg" className="h-110 rounded-2xl border-2 border-black" />
-                        <div className="absolute inset-0 top-60 text-white overlay-1 bg-[#00000081] text-center rounded-b-2xl rounded-t-xl  text-2xl opacity-0 h-50  transition-opacity group-hover:opacity-100 flex flex-col gap-5 justify-center items-center">
-                            <p className="text-2xl font-bold">Milad Azizani</p>
-                            <p className="text-xl font-semibold">MA/Ai Learning</p>
-                            <Button className="h-30 w-30 bg-red-600! font-bold!" type="primary">See Profile</Button>
-                        </div>
-                    </div>
-                </div>
-                <div className=" h-120">
-                    <div className="relative group cursor-pointer">
-                        <img src="./images/amin.jpg" className="h-110 rounded-2xl border-2 border-black" />
-                        <div className="absolute inset-0 top-60 text-white overlay-1 bg-[#00000081] text-center  rounded-b-2xl rounded-t-xl  text-2xl opacity-0 h-50  transition-opacity group-hover:opacity-100 flex flex-col gap-5 justify-center items-center">
-                            <p className="text-2xl font-bold">Amin Rasooli</p>
-                            <p className="text-xl font-semibold">Designer and Mobile Devloper</p>
-                            <Button className="h-30 w-30 bg-red-600! font-bold!" type="primary">See Profile</Button>
-                        </div>
-                    </div>
-                </div>
-                <div className=" h-120">
-                    <div className="relative group cursor-pointer">
-                        <img src="./images/mojib.jpg" className="h-110 rounded-2xl border-2 border-black" />
-                        <div className="absolute inset-0 top-60 text-white overlay-1 bg-[#00000081] text-center  rounded-b-2xl rounded-t-xl  text-2xl opacity-0 h-50  transition-opacity group-hover:opacity-100 flex flex-col gap-5 justify-center items-center">
-                            <p className="text-2xl font-bold">Mojib Movahed</p>
-                            <p className="text-xl font-semibold">Technical Artist and Marketer</p>
-                            <Button className="h-30 w-30 bg-red-600! font-bold!" type="primary">See Profile</Button>
-                        </div>
-                    </div>
-                </div>
-                <div className=" h-120">
-                    <div className="relative group cursor-pointer">
-                        <img src="./images/mojib.jpg" className="h-110 rounded-2xl border-2 border-black" />
-                        <div className="absolute inset-0 top-60 text-white overlay-1 bg-[#00000081] text-center  rounded-b-2xl rounded-t-xl  text-2xl opacity-0 h-50  transition-opacity group-hover:opacity-100 flex flex-col gap-5 justify-center items-center">
-                            <p className="text-2xl font-bold">Nawid Talaye</p>
-                            <p className="text-xl font-semibold">DevOps and Tester</p>
-                            <Button className="h-30 w-30 bg-red-600! font-bold!" type="primary">See Profile</Button>
-                        </div>
-                    </div>
-                </div>
+
+                <ImageCard name={'Mild Azizani'} job={'MA/AI Learning'} src={'./images/milad.jpg'}/>
+                <ImageCard name={'Amin Rasooli'} job={'Designer and Mobile Developer'} src={'/images/amin.jpg'} />
+                <ImageCard name={'Mojib Movahed'} job={'Technical Artist and Marketer'} src={'/images/mojib.jpg'}/>
+                <ImageCard name={'Nawid Talaye'} job={'DevOps and Tester'} src={'/images/mojib.jpg'}/>
+        
 
             </section>
         </main>
