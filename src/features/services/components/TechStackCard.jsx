5b9a89971ce66d59015359deb2ecfc3502704c3e
Milad Azizani <miladazizani@gmail.com>
2026-01-11 09:50:32 +0430

    Add top border to Cards

---
+++ src/features/services/components/TechStackCard.jsx
@@ -0,0 +1,7 @@
+const TechStackCard = () => {
+    return(
+    <>
+    </>
+    )
+}
+export default TechStackCard;
--- src/features/services/pages/Service.jsx
+++ src/features/services/pages/Service.jsx
@@ -12,7 +12,7 @@ const Service = () => {
                 <p className="text-xl font-light opacity-65 ">We build modern, fast, and scalable user interfaces that deliver a smooth and professional experience for users.</p>
             </section>
             <section className="h-8/12 w-full grid grid-cols-3 p-10 gap-10">
-                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer">
+                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer border-t-4 border-blue-600">
                     <div className="w-1/4 flex justify-center items-center">
                         <FaReact className="h-20 w-20 rounded-full hover:text-blue-500 drop-shadow-2xl hover:drop-shadow-blue-400"/>
                     </div>
@@ -21,7 +21,7 @@ const Service = () => {
                     <p className="opacity-65 text-[14px] ">React builds fast, interactive UIs making web pages a little more magical.</p>
                     </div>
                 </div>
-                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer">
+                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer border-t-4 border-orange-600">
                     <div className="w-1/4 flex justify-center items-center">
                         <DiJavascript className="h-20 w-20 rounded-full hover:text-orange-500 drop-shadow-2xl hover:drop-shadow-orange-400"/>
                     </div>
@@ -30,7 +30,7 @@ const Service = () => {
                     <p className="opacity-65 text-[14px] ">JS turns boring websites into dynamic adventures one quirky function at a time.</p>
                     </div>
                 </div>
-                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer">
+                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer border-t-4 border-blue-600">
                     <div className="w-1/4 flex justify-center items-center">
                         <SiTailwindcss className="h-20 w-20 rounded-full hover:text-blue-500 drop-shadow-2xl hover:drop-shadow-blue-400"/>
                     </div>
@@ -39,7 +39,7 @@ const Service = () => {
                     <p className="opacity-65 text-[14px] ">Tailwind designs clean, responsive UIs — faster than your coffee brews.</p>
                     </div>
                 </div>
-                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer">
+                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer border-t-4 border-green-600">
                     <div className="w-1/4 flex justify-center items-center">
                         <SiNextdotjs className="h-20 w-20 rounded-full hover:text-green-500 drop-shadow-lg hover:drop-shadow-green-400"/>
                     </div>
@@ -48,7 +48,7 @@ const Service = () => {
                     <p className="opacity-65 text-[14px] ">Node.js Runs JavaScript on the server turning ideas into live apps.</p>
                     </div>
                 </div>
-                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer">
+                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer border-t-4 border-purple-600">
                     <div className="w-1/4 flex justify-center items-center">
                         <IoLogoFigma className="h-20 w-20 rounded-full hover:text-purple-700 drop-shadow-2xl hover:drop-shadow-purple-400"/>
                     </div>
@@ -57,7 +57,7 @@ const Service = () => {
                     <p className="opacity-65 text-[14px] ">Design, prototype, and collaborate making pixels dance together</p>
                     </div>
                 </div>
-                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer">
+                <div className="h-5/8 flex items-center p-5 gap-5 rounded-xl bg-cyan-50 shadow-2xl hover:-translate-y-3 transition-transform cursor-pointer border-t-4 border-rose-600">
                     <div className="w-1/4 flex justify-center items-center">
                         <DiResponsive className="h-20 w-20 rounded-full hover:text-rose-700 drop-shadow-2xl hover:drop-shadow-rose-400"/>
                     </div>
